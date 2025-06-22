112258a174ad39bca404631ba9782de074f13d63
DFST <mk@dfst.io>
2025-06-22 14:32:09 +0300

    1.0.20

--- packages/abi/src/token/build.ts
+++ packages/abi/src/token/build.ts
@@ -68,7 +68,7 @@ export async function buildTokenLaunchTransaction(params: {
   const sender = PublicKey.fromBase58(args.sender);
   if (nonce === undefined) throw new Error("Nonce is required");
   if (typeof nonce !== "number") throw new Error("Nonce must be a number");
-  const fee = 100_000_000;
+  const fee = 200_000_000;
   if (uri && typeof uri !== "string") throw new Error("Uri must be a string");
   if (!args.tokenAddress || typeof args.tokenAddress !== "string")
     throw new Error("tokenAddress is required");
@@ -394,7 +394,7 @@ export async function buildTokenTransaction(params: {
     bidAddress,
   });
   const memo = args.memo ?? `${txType} ${symbol}`;
-  const fee = 100_000_000;
+  const fee = 200_000_000;
   const provingKey = params.provingKey
     ? PublicKey.fromBase58(params.provingKey)
     : sender;
